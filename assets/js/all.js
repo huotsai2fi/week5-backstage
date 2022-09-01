d5e311506fe4f36edded60e92b5054ea63db5f7c
April Hung <huotsai2fi@gmail.com>
2022-09-01 17:53:26 +0800

    Update 2022-09-01T09:53:09.465Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,8 @@
+"use strict";
+
 $('.sidebar [href]').each(function () {
   if (this.href == window.location.href.split('#')[0]) {
     $(this).addClass('active');
   }
 });
+//# sourceMappingURL=all.js.map
